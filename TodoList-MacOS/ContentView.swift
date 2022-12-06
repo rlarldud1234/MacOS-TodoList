a7d107b081abf6e6d528706129dc3a287a73c4d6
rlarldud1234 <rlarldud6482@naver.com>
2022-12-06 18:02:50 +0900

    ⚙️ :: 프로젝트 생성

---
+++ TodoList-MacOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TodoList-MacOS
+//
+//  Created by 김기영 on 2022/12/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoList-MacOS/TodoList_MacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_MacOSApp.swift
+//  TodoList-MacOS
+//
+//  Created by 김기영 on 2022/12/06.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_MacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
